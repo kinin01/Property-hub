b1754ddced4a6fccff6a3df4ad6e9887ec7c13e7
SenseiKh <hillarykisera@gmail.com>
2025-05-13 20:11:30 +0300

    gix

--- a_core/settings.py
+++ a_core/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 # ALLOWED_HOSTS = getenv('DJANGO_ALLOWED_HOSTS',
 #                        '127.0.0.1,localhost,https://property-hub-s28b.onrender.com,*').split(',')
