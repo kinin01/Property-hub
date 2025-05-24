fd6bfb00ca5dbfc5a90d9437cd4bbdd570e9412e
SenseiKh <hillarykisera@gmail.com>
2025-05-24 18:18:14 +0300

    update on notifications

--- home/serializers.py
+++ home/serializers.py
@@ -6,10 +6,21 @@ from a_users.serializers import CustomUserSerializer
 from rest_framework import serializers
 from .models import Notification
 from a_users.serializers import CustomUserSerializer
+from a_users.models import CustomUser
 
 class NotificationSerializer(serializers.ModelSerializer):
-    user = CustomUserSerializer()
+    recipients = serializers.PrimaryKeyRelatedField(
+        queryset=CustomUser.objects.filter(role='tenant'),
+        many=True,
+        required=False  
+    )
 
     class Meta:
         model = Notification
-        fields = ['message', 'user', 'is_read', 'timestamp']
+        fields = ['id', 'message', 'recipients', 'timestamp', 'is_read', ]
+        read_only_fields = ['timestamp', 'is_read']
+class NotificationMarkReadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notification
+        fields = ['id', 'message', 'recipients', 'timestamp', 'is_read']
+        read_only_fields = ['id', 'message', 'recipients', 'timestamp']
--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import (
-     NotificationListView,NotificationDetailView , home
+from .views import ( NotificationListView, NotificationMarkReadView, TenantNotificationListView, home, NotificationCreateView,
 )
 
 
 urlpatterns = [
     path('', home, name='home'),
-    path('notification/', NotificationListView.as_view(), name='login'),
-    path('notification/<int:pk>/', NotificationDetailView.as_view(), name='tenant-detail'),
-    
+    path('notifications/list/', NotificationListView.as_view(), name='list-notifications'),
+    path('notifications/create/', NotificationCreateView.as_view(), name='create-notification'),
+
+    path('tenant/notifications/', TenantNotificationListView.as_view(), name='tenant-list-notifications'),
+    path('notifications/<int:notification_id>/mark-read/', NotificationMarkReadView.as_view(), name='mark-notification-read'),
 ]
